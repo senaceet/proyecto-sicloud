56f332ecd2b8351479416178ab89c20feb9be6c2
Fabian Lopez <49340662+FabianL6@users.noreply.github.com>
2020-06-25 22:13:47 -0500

    docs: cronograma update

--- 03-Desarrollo/Interface_grafica/rol/admin/iniAdmin.php
+++ 03-Desarrollo/Interface_grafica/rol/admin/iniAdmin.php
@@ -14,20 +14,20 @@ include_once '../../clases/class.usuario.php';
 
 if(isset($_SESSION['usuario'])){
     print_r($_SESSION['usuario']);
+    $usuario = $_SESSION['usuario'];
 
+
+    echo  "Hola: ".$_SESSION['usuario']['ape1'];
+    
+    
+    echo "modulo administracion";
 
 }
 
 
-cardtitulo("Modulo de administracion");
-
-$usuario = $_SESSION['usuario'];
+cardtitulo("Modulo Administrativo");
 
 
-echo  "Hola: ".$_SESSION['usuario']['ape1'];
-
-
-echo "modulo administracion";
 ?>
 
 
@@ -52,71 +52,96 @@ if (isset($_SESSION['message'])) {
 
 <?php $_SESSION['message'] == false; } ?>
 
-<a class = "btn btn-primary"   href="metodos/get.php?cerrarSesion" >Salir de sesion</a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 bg-secondary shadow p-3 mb-5 rounded h-100 py-2 mx-auto">
 
+            <!-- Usuario Card -->
+            <div class="card card-body border">
+                <div class="my-3 shadow p-3 mb-5 bg-white rounded">
+                      
+                    <h4 class="text-center">
+                        <svg class="bi bi-gear-fill" width="1em" height="1em" viewBox="0 0 16 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
+                        </svg>  Configuracion de Administrador</h4></div>
+                <div class="card shadow p-3 mb-5 bg-white rounded">
+                    <div class=" text-center py-2 my-3"><h5>Usuarios</h5></div>
+                        <ul class="list-group list-group-flush">
+                            
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU009-controlUsuarios.php">Administrar solicitudes de usuario</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU006-acomulacionPuntos.php">Acomulacion de puntos</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU005-facturacion.php">Facturacion</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU011-InformeVentas.php">Informe de ventas</a>
+                            </li>
+                        </ul>
+                </div>
 
-<!-- On rows -->
-<tr class="bg-primary">campo 1</tr>
-<tr class="bg-success">...</tr>
-<tr class="bg-warning">...</tr>
-<tr class="bg-danger">...</tr>
-<tr class="bg-info">...</tr>
+                <!-- Productos Card -->
+                <div class="card shadow p-3 mb-5 bg-white rounded">
+                    <div class=" text-center py-2 my-3"><h5>Productos</h5></div>
+                        <ul class="list-group list-group-flush">
+                            
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU004-crearProductos.php">Crear producto</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU003-ingresoProducto.php">Verificar productos</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU0015_16(Administrador)-Solicitudf.php">Solicitudes de Servicios o Productos</a>
+                            </li>
+                            <li class="list-group-item">
+                            <svg class="bi bi-arrow-right-square-fill mr-1" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm5.646 10.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z"/>
+                            </svg>
+                                <a class="text-dark" href="../../CU012-InformeBodega.php">Informe de Bodega</a>
+                            </li>
+                        </ul>
+                </div>
 
-<!-- On cells (`td` or `th`) -->
-<tr>
-  <td class="bg-primary">...</td>
-  <td class="bg-success">...</td>
-  <td class="bg-warning">...</td>
-  <td class="bg-danger">...</td>
-  <td class="bg-info">...</td>
-</tr>
+                <a href="../../index.php" class="mx-auto">
+                    <button type="button" class="btn btn-primary">
+                    <svg class="bi bi-house-door-fill mr-1" width="1.1em" height="1.1em" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+                      <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                    </svg>Regresar al portal
+                    </button>
+                </a>
+            </div>
 
-
-<div class="col-md-auto p-2 ">
-            <table class=" table table-bordered  table-striped bg-white table-sm mx-auto   text-center">
-                <thead>
-
-                    <tr>
-                        <th>Rut</th>
-                        <th>Nombre</th>
-                        <th>Accion</th>
-                        <?php
-
-
-                        //$medida = Medida::ningunDatoM();
-                        $datos  = Empresa::verEmpresa();
-                        if (isset($datos)) {
-                            while ($row = $datos->fetch_array()) {
-                        ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Los nombres que estan son los mismos de los atributos de la base de datos de lo contrario dara un error -->
-                    <td class="bg-<?php  echo $_SESSION['cont']  ?>"     ><?php echo $row['ID_rut'] ?></td>
-                    <td><?php echo $row['nom_empresa'] ?></td>
-
-
-                    <!-- formEdicion.php?accion=editarMedia&&id=<?php// echo $row['ID_medida'] ?> -->
-                    <td>
-                        <a href="   ../forms/formEdicionEmpresa.php?id=<?php echo $row['ID_rut'] ?> " class="btn btn-circle btn-secondary"><i class="fas fa-marker"></i></a>
-                        <a href="../metodos/get.php?accion=eliminarEmpresa&&id=<?php echo $row['ID_rut'] ?>" class="btn btn-circle btn-danger"><i class="far fa-trash-alt"></i></a>
-
-                        <!--  get.php?accion=editarMedia?id= -->
-                        <!-- edit.php?id=<?//php  echo  $row['id_sitio']  ?> -->
-                    </td>
-                </tbody>
-        <?php
-
-                            } //fin del while
-
-
-
-                        }
-        ?>
-            </table>
-
-
-
+            
+            
+        </div>
+    </div>
+</div>
 <?php
 
 include_once '../../plantillas/cuerpo/finhtml.php';
