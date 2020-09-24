528047de8ea59add1303668ecc5f0d6ad142d5d0
FabianL6 <falopez736@misena.edu.co>
2020-09-24 18:55:56 -0500

    shore: MVC FINAL

---
+++ 03-Desarrollo/02)_Interface_grafica/mvc/global/api.php
@@ -0,0 +1,130 @@
+<?php
+// http://localhost/javier/global/appi.php?apicall=readusuarios
+require_once '../controlador/get.php';
+
+// se pasan los parametros requeridos a esta funcion
+function isTheseParametersAvailable($params){
+   $avaible = true;
+   $missingparams = '';
+
+   foreach($params as $param){
+      if(!isset($_POST[$param]) || strlen($_POST[$param]) <=0 ){
+         $avaible = false;
+         $missingparams = $missingparams.','.$param;
+      }
+   }
+   // Si faltan parametros
+   if(!$avaible){
+      $response =[];
+      $response['error'] = true;
+      $response['message'] = 'Parametros:'.substr($missingparams, 1, strlen($missingparams)).'vacion'; 
+      // Error de visualizacion
+      echo json_encode($response);
+      // detener la ejecucion adicional
+      die();
+   }
+}
+
+   //Una matriz que muestra la respuesta de la api
+   $response = [];
+
+   /*
+   Si se trata de una llamada api
+   que significa que un parameetro get llamado se establece una URL
+   y con estos parametros estamos concluyendo que es una llamada api
+   */
+
+   if(isset($_GET['apicall'])){
+      // Aqui van todos los llamados de la api
+      switch ($_GET['apicall']) {
+
+         // Opcion crear usuarios
+         case 'createusuario':
+            // Primero haremos la verificacion de parametros.
+            isTheseParametersAvailable(  [ 'ID_us','nom1','nom2', 'ape1', 'ape2','fecha', 'pass', 'foto', 'correo','FK_tipo_doc' ]  );
+            $db = new ControllerDoc();
+            $result = $db->createUsuariosController(
+                  $_POST['ID_us'], 
+                  $_POST['nom1'],
+                  $_POST['nom2'],
+                  $_POST['ape1'],
+                  $_POST['ape2'],
+                  $_POST['fecha'],    
+                  $_POST['pass'],
+                  $_POST['foto'],
+                  $_POST['correo'],
+                  $_POST['FK_tipo_doc']
+            );  
+
+            if($result){
+               //esto significa que no hay ningun error
+               $response['error'] = false;
+               //Mensaje que ejecuto correctamente 
+               $response['message'] = 'Usuario agregado correctamente';
+               $response['contenido'] = $db->readUsuariosController(
+                  $_POST['ID_us'], 
+                  $_POST['nom1'],
+                  $_POST['nom2'],
+                  $_POST['ape1'],
+                  $_POST['ape2'],
+                  $_POST['fecha'],    
+                  $_POST['pass'] 
+               );  
+            }else{
+               $response['error']   = true;
+               $response['message'] = 'ocurrio un error, intenta nuevamente';
+            }
+            break;
+
+            case 'readusuario';
+               $db = new ControllerDoc();
+               $response['error'] = false;
+               $response['message'] = 'Solicitud completada correctamente';
+               $response['contenido'] = $db->readUsuariosController();
+              // $db->ver($db->readUsuariosController(), 1);
+            break;
+
+            /* case 'loginusuario':
+               isTheseParametersAvailable( ['nDoc', 'pass', 'tDoc'] );
+               $db = new ControllerDoc();
+               $result = $db->loginUsuarioController(
+                  $_POST['nDoc'],
+                  $_POST['pass'],
+                  $_POST['tDoc']);
+
+                 
+
+               if(!$result){
+                 // echo 'no hay datos';
+                  $response['error']      = true;
+                  $response['menssage']   = 'credenciales no validas';
+                  
+               }else{
+                 // echo 'inicio sesion';
+                  $response['error']      = false;
+                  $response['message']    = 'Bienvenido'; 
+                  $response['contenido']  = $result;
+
+                  
+               }
+            break;
+         */
+         default:
+            # code...
+            break;
+      }
+   }else{
+      // Si no es un api el que se estaq invocando
+      // Empujar los valores apropiados en la consulta json
+      $response['message'] = 'Llamado invalido del api';
+   }
+   echo json_encode($response);
+
+
+
+
+
+
+
+
+?>
