c106fbdefc08f508ca2c10fc9f578ec1362b44ff
Javier <jhreyes483@misena.edu.co>
2021-06-08 21:57:27 -0500

    Shore

--- EJECUTABLE-FINAL/proyecto/_models/old/login.php
+++
@@ -1,9 +0,0 @@
-<?
-class login extends Model{
-   function __construct(){
-      parent::__construct();
-      $this->_odbc = $this->loadModel('mySQL', false, 'ineditto'); 
-      $con  = $this->loadModel('consultas.admon', 'c_inicial');
-   }
-   
-}
